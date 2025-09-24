37b6522b84626111227605c2605b82ff4b40f80b
Khaing Thin Zar Sein <khaingthinzarsein03@gmail.com>
2025-09-24 13:49:45 +0700

    Update switch.tsx

--- colavo/components/ui/switch.tsx
+++ colavo/components/ui/switch.tsx
@@ -26,7 +26,7 @@ const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
         />
         <div
           className={cn(
-            "relative w-11 h-6 bg-muted peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary/20 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-muted-foreground/20 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary after:shadow-sm",
+            "relative w-11 h-6 bg-muted border-2 border-border peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary/20 rounded-full peer peer-checked:after:translate-x-[18px] rtl:peer-checked:after:-translate-x-[18px] peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-2 after:border-muted-foreground/40 after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-primary peer-checked:border-primary after:shadow-sm",
             className
           )}
         ></div>
